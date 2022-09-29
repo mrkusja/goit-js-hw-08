60227332f9c34430a6dfb044daca4bb6b4c86c62
Yury Kusoits <kusoitsyury@gmail.com>
2022-09-29 19:47:33 +0200

    fix task 2 and add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,40 +11,30 @@ const throttle = require('lodash.throttle');
 // });
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-window.addEventListener('DOMContentLoaded', startPlayingVideoFromCurrentTime)
+player.on('timeupdate', throttle(onSaveTime, 1000));
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-
-player.on('timeupdate', throttle(savePlayingTimeHandler, 1000))
-
-function savePlayingTimeHandler(data) { 
-    localStorage.setItem("videoplayer-current-time", `${data.seconds}`)
-    
+function onSaveTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 }
 
-function startPlayingVideoFromCurrentTime() {
-    let currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
-    player.setCurrentTime(currentTime).then(function (seconds) {
-        // seconds = the actual time that the player seeked to
-    }).catch(function (error) {
-        switch (error.name) {
-            case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
-                break;
+const startTime = Number(localStorage.getItem('videoplayer-current-time'));
+console.log(typeof startTime);
 
-            default:
-                // some other error occurred
-                break;
-        }
-    })
-  window.removeEventListener('DOMContentLoaded', startPlayingVideoFromCurrentTime)
-}
+player
+  .setCurrentTime(startTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const inputEmail = document.querySelector('input');
+const inputMessage = document.querySelector('textarea');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  const dataForm = {
+    email: inputEmail.value,
+    message: inputMessage.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+}
+
+textForm();
+function textForm() {
+  const savedStoreData = localStorage.getItem('feedback-form-state');
+  if (savedStoreData) {
+    const parceSavedStoreData = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    // console.log(parceSavedStoreData)
+    inputEmail.value = parceSavedStoreData.email;
+    inputMessage.value = parceSavedStoreData.message;
+  }
+}
+
+form.addEventListener('submit', submitHandler);
+
+function submitHandler(event) {
+  event.preventDefault();
+  const resultObject = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(resultObject);
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
